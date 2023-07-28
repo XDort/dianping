0cc126fea2b6e5412cedb0e265dcf678348d908b
XDort <128454183+XDort@users.noreply.github.com>
2023-07-28 20:47:50 +0800

    Update RedissonConfig.java

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -12,7 +12,7 @@ public class RedissonConfig {
     @Bean
     public RedissonClient RedissonClient(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("123456");
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setPassword("xxxx");
         return Redisson.create(config);
     }
 }
